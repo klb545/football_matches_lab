aea814faff619580e8c0a70f2f6fe3aaf306fb72
klb545 <126502552+klb545@users.noreply.github.com>
2023-04-13 20:29:15 +0100

    fixed task 4, annotated answers, completed task 10

--- football_queries.sql
+++ football_queries.sql
@@ -1,85 +1,27 @@
+-- Each of the questions/tasks below can be answered using a `SELECT` query.
+-- When you find a solution copy it into the code block under the question before pushing your solution to GitHub.
 
--- CREATE TABLE football ( -- note: uppercase is convention but not standard
---     id SERIAL, -- i.e. going to be a unique interger.... .... will automatically generate the values in the columns
---     title VARCHAR(255), -- so far, we've got an id field and title field ... note: varchar is a string and 255 is the number of characters - the memory allocated
---     duration INT,
---     rating VARCHAR(10)
--- );
+-- 1) Find all the matches from 2017.
+SELECT *
+FROM matches
+WHERE season = 2017;
 
--- SELECT * FROM matches WHERE season = 2017;
--- SELECT * FROM matches WHERE hometeam = 'Barcelona' OR awayteam = 'Barcelona';
-
--- names of scottish divisions included
--- SELECT name FROM divisions WHERE country = 'Scotland';
-
-
--- Find the value of the `code` for the `Bundesliga` division.
--- Use that code to find out how many matches Freiburg have played in that division.
--- HINT: You will need to query both tables
--- SELECT code FROM divisions WHERE name = 'Bundesliga';
--- SELECT COUNT(hometeam = 'Freiburg') FROM matches WHERE division_code = 'D1';
+-- 2) Find all the matches featuring Barcelona.
+SELECT *
+FROM matches
+WHERE hometeam = 'Barcelona'
+OR awayteam = 'Barcelona';
 
 
-
--- SELECT DISTINCT hometeam FROM matches WHERE (LOWER(hometeam) LIKE LOWER('%City%')) OR (LOWER(hometeam) LIKE LOWER('%City%'));
-
-
-
--- How many different teams have played in matches recorded in a French division?
-
--- SELECT DISTINCT code FROM divisions WHERE country = 'France';
--- SELECT COUNT(DISTINCT hometeam) FROM matches WHERE division_code = 'F1' OR division_code = 'F2';
-
--- Have Huddersfield played Swansea in any of the recorded matches?
--- SELECT COUNT(id) FROM matches WHERE (hometeam = 'Huddersfield' AND awayteam = 'Swansea') OR (hometeam = 'Swansea' AND awayteam = 'Huddersfield');
+-- 3) What are the names of the Scottish divisions included?
+--      answer:  Scottish Championship, Scottish League One, & Scottish Premiership
+SELECT DISTINCT name
+FROM divisions
+WHERE country = 'Scotland';
 
 
--- How many draws were there in the `Eredivisie` between 2010 and 2015?
--- SELECT DISTINCT code
--- FROM divisions
--- WHERE name = 'Eredivisie';
-
--- SELECT COUNT(id)
--- FROM matches
--- WHERE division_code = 'N1'
--- AND (season BETWEEN 2010 AND 2015)
--- AND (ftr = 'D');
-
-
-
-
--- Select the matches played in the Premier League
--- in order of total goals scored from highest to lowest.
--- When two matches have the same total the match with more home goals should come first.
-
-
--- SELECT DISTINCT code FROM divisions WHERE name = 'Premier League';
--- SELECT * FROM matches WHERE division_code = 'E0' ORDER BY ((fthg + ftag), fthg) DESC;
-
-
-
--- In which division and which season were the most goals scored?
--- division with most goals scored
--- SELECT DISTINCT division_code, SUM(division_code.fthg) FROM matches GROUP BY division_code;
--- SELECT SUM(fthg) AS MostGoalsScored FROM matches GROUP BY division_code;
--- SELECT SUM(fthg) FROM matches WHERE division_code = 'E0';
--- SELECT MAX(COUNT(id)*SUM(fthg)) AS MostGoalsScored FROM matches;
--- SELECT division_code FROM matches WHERE MostGoalsScored = 13;
--- season with most goals scoredd
--- SELECT division_code FROM matches WHERE 
-
--- I've pasted in your code and I'm getting two D1s... I think you can fix that by doing
--- `SELECT DISTINCT code
--- FROM divisions
--- WHERE name = 'Bundesliga';` 
-
-
-
-
--- 4) Find the value of the `code` for the `Bundesliga` division.
--- Use that code to find out how many matches Freiburg have played in that division.
--- HINT: You will need to query both tables
-
+-- 4) Find the value of the `code` for the `Bundesliga` division. Use that code to find out how many matches Freiburg have played in that division. HINT: You will need to query both tables
+--      answer: 544
 SELECT DISTINCT code
 FROM divisions
 WHERE name LIKE '%Bundesliga%';
@@ -90,14 +32,85 @@ WHERE (division_code = 'D1' OR division_code = 'D2')
 AND (hometeam = 'Freiburg' OR awayteam = 'Freiburg');
 
 
+-- 5) Find the teams which include the word "City" in their name. 
+--      answer: Bath City, Man City, Edinburgh City, & Bristol City
+SELECT DISTINCT hometeam
+FROM matches
+WHERE (LOWER(hometeam) LIKE LOWER('%City%'))
+OR (LOWER(hometeam) LIKE LOWER('%City%'));
 
 
+-- 6) How many different teams have played in matches recorded in a French division?
+--      answer: 61
+SELECT DISTINCT code
+FROM divisions
+WHERE country = 'France'; -- found to have division codes of 'F1' & 'F2'
+
+SELECT COUNT(DISTINCT hometeam)
+FROM matches
+WHERE division_code = 'F1'
+OR division_code = 'F2';
 
 
+-- 7) Have Huddersfield played Swansea in any of the recorded matches?
+--      answer: yes, 12
+SELECT COUNT(id)
+FROM matches
+WHERE (hometeam = 'Huddersfield' AND awayteam = 'Swansea')
+OR (hometeam = 'Swansea' AND awayteam = 'Huddersfield');
 
 
+-- 8) How many draws were there in the `Eredivisie` between 2010 and 2015?
+--      answer: 431
+SELECT DISTINCT code
+FROM divisions
+WHERE name = 'Eredivisie'; -- found to have a division code of 'N1'
+
+SELECT COUNT(id)
+FROM matches
+WHERE division_code = 'N1'
+AND (season BETWEEN 2010 AND 2015)
+AND (ftr = 'D');
 
 
+-- 9) Select the matches played in the Premier League in order of total goals scored from highest to lowest. When two matches have the same total the match with more home goals should come first.
+SELECT DISTINCT code
+FROM divisions
+WHERE name = 'Premier League'; -- found to have a division code of 'E0'
+
+SELECT *
+FROM matches
+WHERE division_code = 'E0'
+ORDER BY ((fthg + ftag), fthg) DESC;
 
 
+-- 10) In which division and which season were the most goals scored?
 
+--      DIVSION | answer: EFL Championship
+SELECT division_code, SUM(fthg) AS MostGoalsScored
+FROM matches
+GROUP BY division_code
+ORDER BY SUM(fthg) DESC
+LIMIT 1;
+
+SELECT DISTINCT name
+FROM divisions
+WHERE code = 'E1';
+
+--      SEASON | answer: 2012
+SELECT season, SUM(fthg) AS MostGoalsScored
+FROM matches
+GROUP BY season
+ORDER BY SUM(fthg) DESC
+LIMIT 1;
+
+--      DIVISION & SEASON | answer: National League
+SELECT division_code, season, SUM(fthg) AS MostGoalsScored
+FROM matches
+GROUP BY division_code, season
+ORDER BY SUM(fthg) DESC
+LIMIT 1;
+
+SELECT DISTINCT name
+FROM divisions
+WHERE code = 'EC';
--- tasks.md
+++ tasks.md
@@ -2,10 +2,10 @@
 
 Each of the questions/tasks below can be answered using a `SELECT` query. When you find a solution copy it into the code block under the question before pushing your solution to GitHub.
 
+
 1) Find all the matches from 2017.
 
 ```sql
-<!-- Copy solution here -->
 SELECT *
 FROM matches
 WHERE season = 2017;
@@ -15,7 +15,6 @@ WHERE season = 2017;
 2) Find all the matches featuring Barcelona.
 
 ```sql
-<!-- Copy solution here -->
 SELECT *
 FROM matches
 WHERE hometeam = 'Barcelona'
@@ -27,7 +26,7 @@ OR awayteam = 'Barcelona';
 3) What are the names of the Scottish divisions included?
 
 ```sql
-<!-- Copy solution here -->
+-- answer:  Scottish Championship, Scottish League One, & Scottish Premiership
 SELECT name
 FROM divisions
 WHERE country = 'Scotland';
@@ -37,24 +36,22 @@ WHERE country = 'Scotland';
 4) Find the value of the `code` for the `Bundesliga` division. Use that code to find out how many matches Freiburg have played in that division. HINT: You will need to query both tables
 
 ```sql
-<!-- Copy solution here -->
+-- answer: 544
 SELECT DISTINCT code
 FROM divisions
-WHERE name LIKE '%Bundesliga%';
+WHERE name LIKE '%Bundesliga%'; -- found to have division codes of 'D1' & 'D2'
 
 SELECT COUNT(id)
 FROM matches
 WHERE (division_code = 'D1' OR division_code = 'D2')
 AND (hometeam = 'Freiburg' OR awayteam = 'Freiburg');
 
-
-
 ```
 
 5) Find the teams which include the word "City" in their name. 
 
 ```sql
-<!-- Copy solution here -->
+-- answer: Bath City, Man City, Edinburgh City, & Bristol City
 SELECT DISTINCT hometeam
 FROM matches
 WHERE (LOWER(hometeam) LIKE LOWER('%City%'))
@@ -62,13 +59,14 @@ OR (LOWER(hometeam) LIKE LOWER('%City%'));
 
 ```
 
+
 6) How many different teams have played in matches recorded in a French division?
 
 ```sql
-<!-- Copy solution here -->
+-- answer: 61
 SELECT DISTINCT code
 FROM divisions
-WHERE country = 'France';
+WHERE country = 'France'; -- found to have division codes of 'F1' & 'F2'
 
 SELECT COUNT(DISTINCT hometeam)
 FROM matches
@@ -81,7 +79,7 @@ OR division_code = 'F2';
 7) Have Huddersfield played Swansea in any of the recorded matches?
 
 ```sql
-<!-- Copy solution here -->
+-- answer: yes, they played 12 matches together
 SELECT COUNT(id)
 FROM matches
 WHERE (hometeam = 'Huddersfield' AND awayteam = 'Swansea')
@@ -93,10 +91,10 @@ OR (hometeam = 'Swansea' AND awayteam = 'Huddersfield');
 8) How many draws were there in the `Eredivisie` between 2010 and 2015?
 
 ```sql
-<!-- Copy solution here -->
+-- answer: 431
 SELECT DISTINCT code
 FROM divisions
-WHERE name = 'Eredivisie';
+WHERE name = 'Eredivisie'; -- found to have a division code of 'N1'
 
 SELECT COUNT(id)
 FROM matches
@@ -109,10 +107,9 @@ AND (ftr = 'D');
 9) Select the matches played in the Premier League in order of total goals scored from highest to lowest. When two matches have the same total the match with more home goals should come first.
 
 ```sql
-<!-- Copy solution here -->
 SELECT DISTINCT code
 FROM divisions
-WHERE name = 'Premier League';
+WHERE name = 'Premier League'; -- found to have a division code of 'E0'
 
 SELECT *
 FROM matches
@@ -124,8 +121,34 @@ ORDER BY ((fthg + ftag), fthg) DESC;
 10) In which division and which season were the most goals scored?
 
 ```sql
-<!-- Copy solution here -->
+-- DIVSION WITH MOST GOALS | answer: EFL Championship
+SELECT division_code, SUM(fthg) AS MostGoalsScored
+FROM matches
+GROUP BY division_code
+ORDER BY SUM(fthg) DESC
+LIMIT 1;
 
+SELECT DISTINCT name
+FROM divisions
+WHERE code = 'E1';
+
+-- SEASON WITH MOST GOALS | answer: 2012
+SELECT season, SUM(fthg) AS MostGoalsScored
+FROM matches
+GROUP BY season
+ORDER BY SUM(fthg) DESC
+LIMIT 1;
+
+-- DIVISION & SEASON WITH MOST GOALS | answer: National League
+SELECT division_code, season, SUM(fthg) AS MostGoalsScored
+FROM matches
+GROUP BY division_code, season
+ORDER BY SUM(fthg) DESC
+LIMIT 1;
+
+SELECT DISTINCT name
+FROM divisions
+WHERE code = 'EC';
 
 ```
 
